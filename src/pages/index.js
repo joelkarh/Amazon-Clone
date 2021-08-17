4371b3339d21273d5f1bbed44b8198a330ceead4
Joël Karhambae <joelkarhamba@hotmail.com>
2021-08-17 13:28:27 +0200

    Title: Amazone header
    added header.js :construction:

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head>
+      {/* Header */}
+      <Header/>
+      <main>
+        {/* Banner */}
+        {/* ProductFeed */}
+      </main>
+    </div>
+  );
+}
+//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  
